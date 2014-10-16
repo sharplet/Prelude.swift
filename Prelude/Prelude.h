55bcc036dc3f4a4f468516946cd9d84ac3711b39
Adam Sharp <adsharp@me.com>
2014-10-16 17:58:51 +1100

    Initial Commit

---
+++ Prelude/Prelude.h
@@ -0,0 +1,19 @@
+//
+//  Prelude.h
+//  Prelude
+//
+//  Created by Adam Sharp on 16/10/2014.
+//  Copyright (c) 2014 Adam Sharp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Prelude.
+FOUNDATION_EXPORT double PreludeVersionNumber;
+
+//! Project version string for Prelude.
+FOUNDATION_EXPORT const unsigned char PreludeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Prelude/PublicHeader.h>
+
+
